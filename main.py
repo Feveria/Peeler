2c4d2387acd5974676bcd300371a6e34c0c69d37
Artur Szcześniak <feveria@gmail.com>
2016-12-02 12:38:08 +0100

    Basic HTTP response reading and XML files writing implementation.

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
 # Peeler - the scenemusic.net database rip engine.
 # ©2016 Artur Szczesniak
 
+import requests
+import time
+import threading
+
 def start():
+    nectarine_url = "http://www.scenemusic.net/demovibes/xml/song/"
+    songsdir = "songs"
+    songstart = 1
+    songend = 44000
+    for song_number in range(songstart, songend):
+        while True:
+            try:
+                response = requests.get(nectarine_url + str(song_number))
+                response.raise_for_status()
+                print("Current song: {}".format(song_number))
+                with open(songsdir + "/" + str(song_number) + ".xml", "w+") as xml_file:
+                    xml_file.write(response.text.replace("&#39;","\'").replace("&lt;","\<").replace("&gt;","\>"))
+            except requests.ConnectionError as e:
+                print("Connection error: \"{}(...)\", retrying in 5s...".format(str(e.args)[0:50]))
+                time.sleep(5)
+                continue
+            except requests.HTTPError as he:
+                print("HTTP error: \"{}(...)\", skipping song {}.".format(str(he.args)[0:50], song_number))
+                break
+            break
+
     print("/start placeholder/")
 
-
-def welcome_message():
+def welcome_menu():
     made_choice = False
     print("Welcome to Peeler!",
           "Do you want to start this script? (y/n)\n")
@@ -21,6 +45,5 @@ def welcome_message():
         else:
             print("Please type \"y\" or \"n\".")
 
-
 if __name__ == "__main__":
-    welcome_message()
+    welcome_menu()
